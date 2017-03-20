1c3bcc28990d9296f4adf36025d762191ccd3fcb
Raghav Sharma <raghavsharma582@gmail.com>
2017-03-20 22:58:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,30 +15,8 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
 });
 
-var app = express();
-app.use(morgan('combined'));
-
-counter=0;
-app.get('/counter',function(req,res){
-   counter=counter+1;
-   res.send(counter.toString());
-});
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
-});
-
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -51,8 +29,8 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
