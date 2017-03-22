8e9d9c1679a1ab34527666574ab1444bf4fcc5f7
Raghav Sharma <raghavsharma582@gmail.com>
2017-03-23 02:19:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
 
-function hash(input,salt) { 
-var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512'); 
-return hashed.toString('hex');
-
-}
-
 app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/hash/:input', function (req, res) {
-var hashedString=hash(req.params.input,'this-is-a-random-string');
-res.send(hashedString);
+app.get('/article-one',function(req,res){
+    res.send('article one send here');
+});
+
+app.get('/article-two',function(req,res){
+    res.send('article two send here');
+});
+
+app.get('/article-three',function(req,res){
+    res.send('article three send here');
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
 });
